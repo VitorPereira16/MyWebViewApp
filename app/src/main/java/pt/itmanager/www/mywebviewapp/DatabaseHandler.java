94c77297d1523c7d79793bdad3989f94c70bf25f
VitorPereira16 <vitor_pereira_16@sapo.pt>
2017-10-04 12:09:32 +0100

    Comit 1
    Falta gravar o que vem do webservice

--- app/src/main/java/pt/itmanager/www/mywebviewapp/DatabaseHandler.java
+++ app/src/main/java/pt/itmanager/www/mywebviewapp/DatabaseHandler.java
@@ -6,8 +6,10 @@ import java.util.List;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -23,8 +25,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
 	// Contacts Table Columns names
 	private static final String KEY_ID = "id";
-	private static final String KEY_NAME = "name";
-	private static final String KEY_PH_NO = "phone_number";
+	private static final String KEY_NR_FUNC = "numero_funcionario";
+	private static final String KEY_DEPARTAMENTO = "departamento";
+	private static final String KEY_TELEMOVEL = "telemovel";
+	private static final String KEY_EXT_TELEMOVEL = "ext_telemovel";
+	private static final String KEY_TELEFONE = "telefone";
+	private static final String KEY_EXT_TELEFONE = "ext_telefone";
+	private static final String KEY_MAIL = "mail";
+	private static final String KEY_NOME = "nome";
+	private static final String KEY_ULTIMO_NOME = "ultimonome";
+
 
 	public DatabaseHandler(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,8 +44,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
-				+ KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
-				+ KEY_PH_NO + " TEXT" + ")";
+				+ KEY_ID + " INTEGER PRIMARY KEY," + KEY_NR_FUNC + " TEXT,"
+				+ KEY_DEPARTAMENTO + " TEXT,"
+				+ KEY_TELEMOVEL + " TEXT,"
+				+ KEY_EXT_TELEMOVEL + " TEXT,"
+				+ KEY_TELEFONE + " TEXT,"
+				+ KEY_EXT_TELEFONE + " TEXT,"
+				+ KEY_MAIL + " TEXT,"
+				+ KEY_NOME + " TEXT,"
+				+ KEY_ULTIMO_NOME + " TEXT" +")";
 		db.execSQL(CREATE_CONTACTS_TABLE);
 	}
 
@@ -58,8 +75,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		SQLiteDatabase db = this.getWritableDatabase();
 
 		ContentValues values = new ContentValues();
-		values.put(KEY_NAME, contact.getName()); // Contact Name
-		values.put(KEY_PH_NO, contact.getPhoneNumber()); // Contact Phone
+		values.put(KEY_NR_FUNC, contact.getNumeroFuncionario()); // Contact Name
+		values.put(KEY_DEPARTAMENTO, contact.getDepartamento()); // Contact Phone
+		values.put(KEY_TELEMOVEL, contact.getTelemovel()); // Contact Phone
+		values.put(KEY_EXT_TELEMOVEL, contact.getExtTelemovel()); // Contact Phone
+		values.put(KEY_TELEFONE, contact.getTelefone()); // Contact Phone
+		values.put(KEY_EXT_TELEFONE, contact.getExtTelefone()); // Contact Phone
+		values.put(KEY_MAIL, contact.getMail()); // Contact Phone
+		values.put(KEY_NOME, contact.getNome()); // Contact Phone
+		values.put(KEY_ULTIMO_NOME, contact.getUltimoNome()); // Contact Phone
 
 		// Inserting Row
 		db.insert(TABLE_CONTACTS, null, values);
@@ -71,13 +95,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		SQLiteDatabase db = this.getReadableDatabase();
 
 		Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID,
-				KEY_NAME, KEY_PH_NO }, KEY_ID + "=?",
+						KEY_NR_FUNC, KEY_DEPARTAMENTO, KEY_TELEMOVEL, KEY_EXT_TELEMOVEL, KEY_TELEFONE, KEY_EXT_TELEFONE, KEY_MAIL, KEY_NOME, KEY_ULTIMO_NOME }, KEY_ID + "=?",
 				new String[] { String.valueOf(id) }, null, null, null, null);
 		if (cursor != null)
 			cursor.moveToFirst();
 
 		Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
-				cursor.getString(1), cursor.getString(2));
+				cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), cursor.getString(8), cursor.getString(9));
 		// return contact
 		return contact;
 	}
@@ -87,13 +111,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		SQLiteDatabase db = this.getReadableDatabase();
 
 		Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID,
-						KEY_NAME, KEY_PH_NO }, KEY_PH_NO + "=?",
+				KEY_NR_FUNC, KEY_DEPARTAMENTO, KEY_TELEMOVEL, KEY_EXT_TELEMOVEL, KEY_TELEFONE, KEY_EXT_TELEFONE, KEY_MAIL, KEY_NOME, KEY_ULTIMO_NOME }, KEY_TELEMOVEL + "=?",
 				new String[] { String.valueOf(number) }, null, null, null, null);
 		if (cursor != null)
 			cursor.moveToFirst();
 
 		Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
-				cursor.getString(1), cursor.getString(2));
+				cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), cursor.getString(8), cursor.getString(9));
 		// return contact
 		return contact;
 	}
@@ -106,14 +130,21 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
 		SQLiteDatabase db = this.getWritableDatabase();
 		Cursor cursor = db.rawQuery(selectQuery, null);
-
+		Log.d("cor",  DatabaseUtils.dumpCursorToString(cursor));
 		// looping through all rows and adding to list
 		if (cursor.moveToFirst()) {
 			do {
 				Contact contact = new Contact();
 				contact.setID(Integer.parseInt(cursor.getString(0)));
-				contact.setName(cursor.getString(1));
-				contact.setPhoneNumber(cursor.getString(2));
+				contact.setNumeroFuncionario(cursor.getString(1)); // Contact Name
+				contact.setDepartamento(cursor.getString(2)); // Contact Phone
+				contact.setTelemovel(cursor.getString(3)); // Contact Phone
+				contact.setExtTelemovel(cursor.getString(4)); // Contact Phone
+				contact.setTelefone(cursor.getString(5)); // Contact Phone
+				contact.setExtTelefone(cursor.getString(6)); // Contact Phone
+				contact.setMail(cursor.getString(7)); // Contact Phone
+				contact.setNome(cursor.getString(8)); // Contact Phone
+				contact.setUltimoNome(cursor.getString(9)); // Contact Phone
 				// Adding contact to list
 				contactList.add(contact);
 			} while (cursor.moveToNext());
@@ -128,8 +159,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		SQLiteDatabase db = this.getWritableDatabase();
 
 		ContentValues values = new ContentValues();
-		values.put(KEY_NAME, contact.getName());
-		values.put(KEY_PH_NO, contact.getPhoneNumber());
+		values.put(KEY_NR_FUNC, contact.getNumeroFuncionario()); // Contact Name
+		values.put(KEY_DEPARTAMENTO, contact.getDepartamento()); // Contact Phone
+		values.put(KEY_TELEMOVEL, contact.getTelemovel()); // Contact Phone
+		values.put(KEY_EXT_TELEMOVEL, contact.getExtTelemovel()); // Contact Phone
+		values.put(KEY_TELEFONE, contact.getTelefone()); // Contact Phone
+		values.put(KEY_EXT_TELEFONE, contact.getExtTelefone()); // Contact Phone
+		values.put(KEY_MAIL, contact.getMail()); // Contact Phone
+		values.put(KEY_NOME, contact.getNome()); // Contact Phone
+		values.put(KEY_ULTIMO_NOME, contact.getUltimoNome()); // Contact Phone
 
 		// updating row
 		return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?",
--- app/src/main/java/pt/itmanager/www/mywebviewapp/MainActivity.java
+++ app/src/main/java/pt/itmanager/www/mywebviewapp/MainActivity.java
@@ -1,5 +1,6 @@
 package pt.itmanager.www.mywebviewapp;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -22,57 +23,44 @@ import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
-
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
+    //String weatherWebserviceURL = "http://api.openweathermap.org/data/2.5/weather?q=ariana,tn&appid=2156e2dd5b92590ab69c0ae1b2d24586&units=metric";
+    String weatherWebserviceURL = "http://itmanager.pt/mobile_contacts/get_contacts.php";
+
+    private String apiPath = "http://itmanager.pt/mobile_contacts/get_contacts.php";
+    private ProgressDialog processDialog;
+    private JSONArray restulJsonArray;
+    private int success = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new ServiceStubAsyncTask(this, this).execute();
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
@@ -87,18 +75,6 @@ public class MainActivity extends AppCompatActivity {
         mWebView.getSettings().setDatabaseEnabled(true);
         mWebView.getSettings().setDomStorageEnabled(true);
         mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT); // load online by default
-
-        //DatabaseHandler db = new DatabaseHandler(this);
-
-        /**
-         * CRUD Operations
-         * */
-        // Inserting Contacts
-        //Log.d("Insert: ", "Inserting ..");
-        //db.addContact(new Contact("Ravi", "9100000000"));
-        //db.addContact(new Contact("Srinivas", "9199999999"));
-        //db.addContact(new Contact("Tommy", "9522222222"));
-        //db.addContact(new Contact("Karthik", "9533333333"));
 
 
         if (!isNetworkAvailable()) { // loading offline
@@ -136,138 +112,79 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private class ServiceStubAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private Context mContext;
+        private Activity mActivity;
+        String response = "";
+        HashMap<String, String> postDataParams;
+
+        public ServiceStubAsyncTask(Context context, Activity activity) {
+            mContext = context;
+            mActivity = activity;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            postDataParams = new HashMap<String, String>();
+            postDataParams.put("HTTP_ACCEPT", "application/json");
+
+            HttpConnectionService service = new HttpConnectionService();
+            response = service.sendRequest(apiPath, postDataParams);
+            DatabaseHandler db = new DatabaseHandler(mContext);
+            JSONArray jsonArray = null;
+
+            try {
+                jsonArray = new JSONArray(response);
+                Log.d("Tag", "Error:" + jsonArray);
+
+                for(int i = 0; i < jsonArray.length(); i++){
+                    String a = jsonArray.getString(i);
+                    JSONObject resultJsonObject = new JSONObject(a);
+
+
+                    String idd = (String) resultJsonObject.get("id");
+                    String numero_funcionario = (String) resultJsonObject.get("numero_funcionario");
+                    String departamento = (String) resultJsonObject.get("departamento");
+                    String telemovel = (String) resultJsonObject.get("telemovel");
+                    String ext_telemovel = (String) resultJsonObject.get("ext_telemovel");
+                    String telefone = (String) resultJsonObject.get("telefone");
+                    String ext_telefone = (String) resultJsonObject.get("ext_telefone");
+                    String mail = (String) resultJsonObject.get("mail");
+                    String nome = (String) resultJsonObject.get("nome");
+                    //String ultimonome = (String) resultJsonObject.get("ultimonome");
+                    String ultimonome = "";
+
+                    //Contact cn = db.getContact(Integer.parseInt(idd));
+                    //Log.d("Tag", "Error:" + cn);
+                    //db.addContact(new Contact(Integer.parseInt(idd), numero_funcionario, departamento, telemovel, ext_telemovel, telefone, ext_telefone, mail, nome, ultimonome));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            success = 1;
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+
+    }
+
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-
-    // Class with extends AsyncTask class
-
-    private class LongOperation  extends AsyncTask<String, Void, Void> {
-
-        // Required initialization
-
-        private final HttpClient Client = new DefaultHttpClient();
-        private String Content;
-        private String Error = null;
-        private ProgressDialog Dialog = new ProgressDialog(RestFulWebservice.this);
-        String data ="";
-        TextView uiUpdate = (TextView) findViewById(R.id.output);
-        TextView jsonParsed = (TextView) findViewById(R.id.jsonParsed);
-        int sizeData = 0;
-        EditText serverText = (EditText) findViewById(R.id.serverText);
-
-
-        protected void onPreExecute() {
-            // NOTE: You can call UI Element here.
-            //Start Progress Dialog (Message)
-            Dialog.setMessage("Please wait..");
-            Dialog.show();
-            try{
-                // Set Request parameter
-                data +="&" + URLEncoder.encode("data", "UTF-8") + "="+serverText.getText();
-
-            } catch (UnsupportedEncodingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        // Call after onPreExecute method
-        protected Void doInBackground(String... urls) {
-            /************ Make Post Call To Web Server ***********/
-            BufferedReader reader=null;
-            // Send data
-            try
-            {
-                // Defined URL  where to send data
-                URL url = new URL(urls[0]);
-
-                // Send POST data request
-                URLConnection conn = url.openConnection();
-                conn.setDoOutput(true);
-                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-                wr.write( data );
-                wr.flush();
-
-                // Get the server response
-                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                // Read Server Response
-                while((line = reader.readLine()) != null)
-                {
-                    // Append server response in string
-                    sb.append(line + "");
-                }
-                // Append Server Response To Content String
-                Content = sb.toString();
-            }
-            catch(Exception ex)
-            {
-                Error = ex.getMessage();
-            }
-            finally
-            {
-                try
-                {
-                    reader.close();
-                }
-                catch(Exception ex) {}
-            }
-            /*****************************************************/
-            return null;
-        }
-
-        protected void onPostExecute(Void unused) {
-            // NOTE: You can call UI Element here.
-            // Close progress dialog
-            Dialog.dismiss();
-            if (Error != null) {
-                uiUpdate.setText("Output : "+Error);
-            } else {
-                // Show Response Json On Screen (activity)
-                uiUpdate.setText( Content );
-                /****************** Start Parse Response JSON Data *************/
-                String OutputData = "";
-                JSONObject jsonResponse;
-
-                try {
-
-                    /****** Creates a new JSONObject with name/value mappings from the JSON string. ********/
-                    jsonResponse = new JSONObject(Content);
-
-                    /***** Returns the value mapped by name if it exists and is a JSONArray. ***/
-                    /*******  Returns null otherwise.  *******/
-                    JSONArray jsonMainNode = jsonResponse.optJSONArray("Android");
-
-                    /*********** Process each JSON Node ************/
-
-                    int lengthJsonArr = jsonMainNode.length();
-
-                    for(int i=0; i < lengthJsonArr; i++)
-                    {
-                        /****** Get Object for each JSON node.***********/
-                        JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
-
-                        /******* Fetch node values **********/
-                        String name       = jsonChildNode.optString("name").toString();
-                        String number     = jsonChildNode.optString("number").toString();
-                        String date_added = jsonChildNode.optString("date_added").toString();
-
-                        OutputData += " Name: "+ name +" "+ "Number: "+ number +" "+"Time: "+date_added+" "+"--------------------------------------------------";
-                    }
-                    /****************** End Parse Response JSON Data *************/
-                    //Show Parsed Output on screen (activity)
-                    jsonParsed.setText( OutputData );
-                } catch (JSONException e) {
-
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
 }
